cd358fe95e22c738e05089cbf397f0b2529d15d8
byjujubagomes <jujusardynha@hotmail.com>
2023-03-14 13:41:16 -0300

    Gaia

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GaiaSurfeFeminino.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
